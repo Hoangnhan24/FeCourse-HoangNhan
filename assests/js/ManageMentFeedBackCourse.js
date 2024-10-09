e0763f70700c92ebcc29f7b0b1d23956890b2b34
hoangnhan <nhanlekame@gmail.com>
2024-10-09 21:07:27 +0700

    CourseFE

---
+++ assests/js/ManageMentFeedBackCourse.js
@@ -0,0 +1,42 @@
+function toggleLesson(part) {
+    part.classList.toggle('active');
+}
+
+function selectStar(star) {
+    const stars = star.parentElement.querySelectorAll('i');
+    let index = Array.from(stars).indexOf(star);
+
+    if (star.classList.contains('selected')) {
+        stars.forEach(s => s.classList.remove('selected'));
+        console.log(0); // No star selected
+    } else {
+        stars.forEach((s, i) => {
+            if (i <= index) {
+                s.classList.add('selected');
+            } else {
+                s.classList.remove('selected');
+            }
+        });
+        console.log(index + 1); // Log number of stars selected
+    }
+}
+
+function toggleOptionsMenu(icon) {
+    const menu = icon.nextElementSibling;
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+}
+
+function deletePart(partName) {
+    console.log('Deleting part:', partName);
+    // Add functionality to delete part
+}
+
+function deleteLesson(lessonName) {
+    console.log('Deleting lesson:', lessonName);
+    // Add functionality to delete lesson
+}
+
+function deleteComment(commentAuthor) {
+    console.log('Deleting comment by:', commentAuthor);
+    // Add functionality to delete comment
+}
